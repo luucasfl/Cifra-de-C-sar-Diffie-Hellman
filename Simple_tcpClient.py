4d5ba6b35f96fb468b15aef8f98e8410e4bac869
Lucas de Freitas <89170976+luucasfl@users.noreply.github.com>
2024-02-23 19:26:38 -0300

    feat(commit-01): add files

---
+++ Simple_tcpClient.py
@@ -0,0 +1,60 @@
+import random
+from socket import *
+
+G = 17
+N = 127
+Y = random.randint(1, 5000)
+
+# Calcula o valor da chave R2S
+def calcula_r2():
+    return (G ** Y) % N
+
+# Função para criptografar usando a Cifra de César
+def encrypt(text, key):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shifted = (ord(char) + key - 97) % 26 + 97 if char.islower() else (ord(char) + key - 65) % 26 + 65
+            encrypted_text += chr(shifted)
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+# Função para decriptografar usando a Cifra de César
+def decrypt(text, key):
+    decrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shifted = (ord(char) - key - 97) % 26 + 97 if char.islower() else (ord(char) - key - 65) % 26 + 65
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+# Calcula a chave K2
+def calcula_k2(r1):
+    return (r1 ** Y) % N
+
+serverName = "192.168.56.1"
+serverPort = 1300
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+
+# Envio da chave R2 do cliente para o servidor
+r2 = calcula_r2()
+clientSocket.send(str(r2).encode())
+
+# Recebimento da chave R1 do servidor
+r1 = int(clientSocket.recv(1024).decode())
+
+# Criptografia e envio da mensagem usando a chave K2
+k2 = calcula_k2(r1)
+sentence = input("Input lowercase sentence: ")
+encrypted_sentence = encrypt(sentence, k2)
+clientSocket.send(encrypted_sentence.encode())
+
+modifiedSentence = clientSocket.recv(1024)
+text = decrypt(modifiedSentence.decode(), k2)
+print("Received from Make Upper Case Server: ", text)
+clientSocket.close()
---
+++ Simple_tcpServer.py
@@ -0,0 +1,66 @@
+import random
+from socket import *
+
+G = 17
+N = 127
+X = random.randint(1, 5000)
+
+# Calcula o valor da chave R1
+def calcula_r1():
+    return (G ** X) % N
+
+# Função para criptografar usando a Cifra de César
+def encrypt(text, key):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shifted = (ord(char) + key - 97) % 26 + 97 if char.islower() else (ord(char) + key - 65) % 26 + 65
+            encrypted_text += chr(shifted)
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+# Função para decriptografar usando a Cifra de César
+def decrypt(text, key):
+    decrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shifted = (ord(char) - key - 97) % 26 + 97 if char.islower() else (ord(char) - key - 65) % 26 + 65
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+# Calcula o valor da chave K1
+def calcula_k1(r2):
+    return (r2 ** X) % N
+
+serverPort = 1300
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(("", serverPort))
+serverSocket.listen(5) # o argumento “listen” diz à biblioteca de soquetes que queremos enfileirar no máximo 5 requisições de conexão (normalmente o máximo) antes de recusar começar a recusar conexões externas. Caso o resto do código esteja escrito corretamente, isso deverá ser o suficiente.
+print("TCP Server\n")
+connectionSocket, addr = serverSocket.accept()
+
+# Recebimento da chave pública do cliente
+r2 = int(connectionSocket.recv(1024).decode())
+
+# Envio da chave R1 para o client
+r1 = calcula_r1()
+connectionSocket.send(str(r1).encode())
+
+# Descriptografia da mensagem
+k1 = calcula_k1(r2)
+sentence = connectionSocket.recv(1024).decode()
+received = decrypt(sentence, k1)
+print("Received From Client: ", received)
+
+capitalizedSentence = received.upper()  # processamento
+
+# Criptografia da mensagem e envio usando a chave K1
+encrypted_sentence = encrypt(capitalizedSentence, k1)
+connectionSocket.send(encrypted_sentence.encode())
+
+sent = encrypted_sentence
+print("Sent back to Client: ", sent)
+connectionSocket.close()
