7b6b57a2062e8743062e28a724fad59f895efe85
Lucas de Freitas <89170976+luucasfl@users.noreply.github.com>
2024-02-23 20:01:38 -0300

    Update Simple_tcpClient.py

--- Simple_tcpClient.py
+++ Simple_tcpClient.py
@@ -1,3 +1,9 @@
+# Nome                            RA
+# Alessandro Matheus Toledo       082190007
+# Gabriel Tadeu Sperche           082190012
+# Lucas de Freitas Leal           082190025
+# Murilo Moura Macedo             082190034
+
 import random
 from socket import *
 
